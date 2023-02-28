acc6b7b72a86cfc0914e63df50a2fa9fda098600
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-02-28 11:14:40 +0300

    add file upload form

--- FileUpload/Pages/Index.cshtml.cs
+++ FileUpload/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -18,6 +19,11 @@ namespace FileUpload.Pages
         }
 
         public void OnGet()
+        {
+
+        }
+
+        public void OnPost(IFormFile file)
         {
 
         }
